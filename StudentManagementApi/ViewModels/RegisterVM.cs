7f145d9a6a96dd5c7197e99e0d11ba20cc1fefc7
lexpierin <alex_pierini@hotmail.com>
2022-02-27 12:21:07 -0500

    View Models: Login, Register, UserToken

---
+++ StudentManagementApi/ViewModels/RegisterVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementApi.ViewModels
+{
+    public class RegisterVM
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ StudentManagementApi/ViewModels/UserTokenVM.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentManagementApi.ViewModels
+{
+    public class UserTokenVM
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
