4cad103f6a55d5cb1d960f7564914b1643f48c59
lexpierin <alex_pierini@hotmail.com>
2022-03-08 23:50:25 -0500

    AxiosService headers token

--- frontend/src/services/axiosService.js
+++ frontend/src/services/axiosService.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const axiosService = axios.create({
     baseURL: "https://localhost:5001",    
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+    }
 })
 
 export default axiosService;
